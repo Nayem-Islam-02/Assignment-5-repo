1f858ee7ed41232a3d707e4f1927a640b4a870d4
jarif hasan <hasanjarif939@gmail.com>
2024-09-24 01:34:24 +0600

    comment

--- add.js
+++ add.js
@@ -67,6 +67,25 @@ document.getElementById('donate-click-one').addEventListener('click', function(e
     let myCredit = parseFloat(document.getElementById('my-amount').innerText);
     let creditDonate = myCredit - donateInputOne;
     document.getElementById('my-amount').innerText = creditDonate;
+
+    // history part for one
+
+    const historyItem = document.createElement("div");
+
+historyItem.className ="bg-white rounded-md border border-indigo-500";
+
+historyItem.innerHTML= `
+<p class="font-bold px-3 py-2"> ${donateInputOne} Taka is Donate for Flood at Noakhali, Bangladesh</p>
+
+<p class="font-bold px-3 py-2"> Date: ${new Date().toLocaleDateString()}</p>
+`;
+
+const historyContainer = document.getElementById("history-list");
+
+historyContainer.insertBefore(historyItem, historyContainer.firstChild)
+
+
+    
 })
 
 
@@ -95,6 +114,22 @@ document.getElementById('donate-click-two').addEventListener('click', function(e
     let myCredit = parseFloat(document.getElementById('my-amount').innerText);
     let creditDonate = myCredit - donateInputTwo;
     document.getElementById('my-amount').innerText = creditDonate;
+
+    // history for  two
+
+    const historyItem = document.createElement("div");
+
+historyItem.className ="bg-white rounded-md border border-indigo-500";
+
+historyItem.innerHTML= `
+<p class="font-bold px-3 py-2"> ${donateInputTwo}  Taka is Donated for Flood Relief in Feni,Bangladesh</p>
+
+<p class="font-bold px-3 py-2"> Date: ${new Date().toLocaleDateString()}</p>
+`;
+
+const historyContainer = document.getElementById("history-list");
+
+historyContainer.insertBefore(historyItem, historyContainer.firstChild)
 })
 
 // for three
@@ -122,5 +157,47 @@ document.getElementById('donate-click-three').addEventListener('click', function
     let myCredit = parseFloat(document.getElementById('my-amount').innerText);
     let creditDonate = myCredit - donateInputThree;
     document.getElementById('my-amount').innerText = creditDonate;
+
+    // history for three
+
+    const historyItem = document.createElement("div");
+
+historyItem.className ="bg-white rounded-md border border-indigo-500";
+
+historyItem.innerHTML= `
+<p class="font-bold px-3 py-2"> ${donateInputThree}  Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</p>
+
+<p class="font-bold px-3 py-2"> Date: ${new Date().toLocaleDateString()}</p>
+`;
+
+const historyContainer = document.getElementById("history-list");
+
+historyContainer.insertBefore(historyItem, historyContainer.firstChild)
 })
 
+// history button function
+
+const historyTab = document.getElementById('history');
+const donationTab = document.getElementById("donation");
+historyTab.addEventListener('click', function(){
+
+    historyTab.classList.add( 'bg-yellow-200');
+
+
+    donationTab.classList.remove('bg-yellow-200');
+
+    document.getElementById('cards-donation').classList.add('hidden');
+
+    document.getElementById("donationHistory").classList.remove("hidden");
+})
+
+donationTab.addEventListener('click', function(){
+    donationTab.classList.add('bg-yellow-200')
+
+    historyTab.classList.remove('bg-yellow-200');
+
+    document.getElementById('cards-donation').classList.remove('hidden');
+
+    document.getElementById("donationHistory").classList.add("hidden");
+
+})
